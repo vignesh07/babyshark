10e8fa5f60b5eea8dc8dff5edbd5102db157f9c5
vignesh07 <vigneshnatarajan92@gmail.com>
2026-02-21 03:30:40 -0800

    hexdump: dim ascii gutter separator

--- rust/src/ui.rs
+++ rust/src/ui.rs
@@ -434,8 +434,7 @@ fn bytes_to_pretty_lines(bytes: &[u8], highlight: Option<(usize, usize)>) -> Vec
             }
         }
 
-        spans.push(Span::raw("  |"));
-        spans.push(Span::styled(" ", Style::default().fg(c_muted())));
+        spans.push(Span::styled("  | ", Style::default().fg(c_muted()).add_modifier(Modifier::DIM)));
 
         // ascii
         for i in 0..HEXDUMP_COLS {
